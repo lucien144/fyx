05a2e50dcce4c6f0500660fb9160c3c1de616d14
Jan Blasko <honza@lucien144.net>
2019-12-06 09:44:38 +1100

    Load credentials before starting the app...
    Signed-off-by: Jan Blasko <honza@lucien144.net>

--- lib/main.dart
+++ lib/main.dart
@@ -3,20 +3,27 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:fyx/PlatformApp.dart';
 import 'package:fyx/PlatformThemeData.dart';
+import 'package:fyx/controllers/ApiController.dart';
+import 'package:fyx/model/Credentials.dart';
 import 'package:fyx/pages/HomePage.dart';
+import 'package:fyx/pages/LoginPage.dart';
 
-void main() {
+void main() async {
   SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp, DeviceOrientation.portraitDown]);
-  return runApp(FyxApp());
+  return runApp(FyxApp(await ApiController.getCredentials()));
 }
 
 class FyxApp extends StatelessWidget {
+  final Credentials _credentials;
+
+  const FyxApp(this._credentials, {Key key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return PlatformApp(
       title: 'Fyx',
       theme: PlatformThemeData(primaryColor: Color(0xFF009D9D)), // Color schema -> https://mycolor.space/?hex=%231AD592&sub=1
-      home: HomePage(),
+      home: _credentials is Credentials && _credentials.isValid ? HomePage() : LoginPage(),
     );
   }
 }
