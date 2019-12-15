f88dfdc4de80045727c36f3506372357731a7aa1
Jan Blasko <honza@lucien144.net>
2019-12-15 19:17:40 +1100

    Avatar has a border now
    Signed-off-by: Jan Blasko <honza@lucien144.net>

--- lib/components/post/PostAvatar.dart
+++ lib/components/post/PostAvatar.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 
 class PostAvatar extends StatelessWidget {
@@ -9,13 +10,21 @@ class PostAvatar extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Row(children: <Widget>[
-      ClipRRect(
-        borderRadius: BorderRadius.circular(20),
-        child: Image.network(
-          image,
-          fit: BoxFit.cover,
-          width: 40,
-          height: 40,
+      Container(
+        padding: EdgeInsets.all(1),
+        decoration: BoxDecoration(borderRadius: BorderRadius.circular(22), color: Colors.black),
+        child: Container(
+          padding: EdgeInsets.all(2),
+          decoration: BoxDecoration(borderRadius: BorderRadius.circular(22), color: Colors.white),
+          child: ClipRRect(
+            borderRadius: BorderRadius.circular(20),
+            child: Image.network(
+              image,
+              fit: BoxFit.cover,
+              width: 40,
+              height: 40,
+            ),
+          ),
         ),
       ),
       SizedBox(
