6ccf24a8010d7b897df6b8d957eb56335c78af07
Jan Blasko <honza@lucien144.net>
2020-04-13 22:46:22 +0200

    Refactor and startup error fix
    Signed-off-by: Jan Blasko <honza@lucien144.net>

--- lib/model/reponses/BookmarksResponse.dart
+++ lib/model/reponses/BookmarksResponse.dart
@@ -1,21 +1,19 @@
+import 'package:fyx/model/System.dart';
+
 class BookmarksResponse {
   List<dynamic> _discussions;
   List<dynamic> _categories;
-  Map<String, dynamic> _system;
+  System _system;
 
   BookmarksResponse.fromJson(Map<String, dynamic> json) {
-    _discussions = json['data']['discussions'];
-    _categories = json['data']['categories'];
-    _system = json['system'];
-//    TODO
-//    "system": {
-//      "max_file_size": "10485760",
-//      "notice_count": "5",
-//      "premium": "1"
-//    }
+    if (json.containsKey('data') && json['data'] is Map) {
+      _discussions = json['data']['discussions'];
+      _categories = json['data']['categories'];
+      _system = System.fromJson(json['system']);
+    }
   }
 
-  Map<String, dynamic> get system => _system;
+  System get system => _system;
 
   List<dynamic> get discussions => _discussions;
 
