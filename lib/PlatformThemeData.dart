7b750432808a222f236e3be6a67c8dc62514b610
Jan Blasko <honza@lucien144.net>
2020-04-14 09:38:10 +0200

    fixes #37
    Signed-off-by: Jan Blasko <honza@lucien144.net>

--- lib/PlatformThemeData.dart
+++ lib/PlatformThemeData.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:fyx/theme/T.dart';
 
 class PlatformThemeData {
@@ -12,6 +13,8 @@ class PlatformThemeData {
   }
 
   cupertino() {
+    SystemUiOverlayStyle(statusBarBrightness: Brightness.light);
+
     return CupertinoThemeData(
         primaryColor: primaryColor,
         brightness: Brightness.light,
