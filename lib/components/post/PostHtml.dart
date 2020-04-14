acd460a89ce2889e49200296f2337881072f218d
Jan Blasko <honza@lucien144.net>
2020-04-14 23:21:10 +0200

    Spoilers, first version closes #7
    Signed-off-by: Jan Blasko <honza@lucien144.net>

--- lib/components/post/PostHtml.dart
+++ lib/components/post/PostHtml.dart
@@ -1,10 +1,13 @@
+import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:flutter_html/flutter_html.dart';
 import 'package:fyx/PlatformTheme.dart';
 import 'package:fyx/components/post/PostHeroAttachment.dart';
+import 'package:fyx/components/post/Spoiler.dart';
 import 'package:fyx/model/post/Content.dart';
 import 'package:fyx/model/provider/SettingsModel.dart';
 import 'package:fyx/pages/DiscussionPage.dart';
+import 'package:html/dom.dart' as dom;
 import 'package:provider/provider.dart';
 
 class PostHtml extends StatelessWidget {
@@ -17,6 +20,15 @@ class PostHtml extends StatelessWidget {
     return Consumer<SettingsModel>(
         builder: (context, settings, child) => Html(
               data: settings.useHeroPosts ? content.body : content.rawBody,
+              customRender: (dom.Node node, children) {
+                if (node is dom.Element) {
+                  if (node.localName == 'span' && node.classes.contains('spoiler')) {
+                    return Spoiler(node.innerHtml);
+                  }
+                }
+                return null;
+              },
+              useRichText: false,
               onImageTap: (String src) {
                 Navigator.of(context).pushNamed('/gallery', arguments: GalleryArguments(src, images: content.images));
               },
