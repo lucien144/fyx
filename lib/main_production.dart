517da50e9719a1ee52bfef4359f517c0099210c6
Jan Blasko <honza@lucien144.net>
2020-04-18 21:37:25 +0200

    🤦‍♂️ Production build logout fix
    Signed-off-by: Jan Blasko <honza@lucien144.net>

--- lib/main_production.dart
+++ lib/main_production.dart
@@ -2,7 +2,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:fyx/FyxApp.dart';
 
 void main() async {
-  FyxApp.init();
+  await FyxApp.init();
   try {
     return runApp(FyxApp()..setEnv(Environment.production));
   } catch (error) {
