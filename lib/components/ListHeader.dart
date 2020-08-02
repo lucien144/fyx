920968709725c8949843c77ddbe8d939cef275a7
Jan Blasko <honza@lucien144.net>
2020-08-02 15:25:54 +0200

    Close #98

--- lib/components/ListHeader.dart
+++ lib/components/ListHeader.dart
@@ -6,21 +6,25 @@ import 'package:fyx/model/Category.dart';
 
 class ListHeader extends ListItemWithCategory {
   final Category header;
+  final Function onTap;
 
-  ListHeader(this.header);
+  ListHeader(this.header, {this.onTap});
 
   @override
   int get category => header.idCat;
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: PlatformTheme.of(context).primaryColor,
-      padding: EdgeInsets.all(8),
-      alignment: Alignment.centerLeft,
-      child: Text(
-        header.jmeno,
-        style: const TextStyle(color: Colors.white),
+    return GestureDetector(
+      onTap: () => this.onTap is Function ? this.onTap() : null,
+      child: Container(
+        color: PlatformTheme.of(context).primaryColor,
+        padding: EdgeInsets.all(8),
+        alignment: Alignment.centerLeft,
+        child: Text(
+          header.jmeno,
+          style: const TextStyle(color: Colors.white),
+        ),
       ),
     );
   }
