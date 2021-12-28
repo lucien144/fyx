58c9d3898adcd33549f9ce0aa0c86120d3be25c3
Jan Blasko <honza@lucien144.net>
2021-12-28 21:24:18 +0100

    feat: unread stripe #260

--- lib/theme/UnreadBadgeDecoration.dart
+++ lib/theme/UnreadBadgeDecoration.dart
@@ -21,20 +21,11 @@ class _UnreadBadgePainter extends BoxPainter {
   void paint(Canvas canvas, Offset offset, ImageConfiguration configuration) {
     canvas.save();
     canvas.translate(0, offset.dy);
-    canvas.drawPath(buildBadgePath(), getBadgePaint());
+    canvas.drawPath(Path()..addRect(Rect.fromLTRB(0, 0, 5, configuration.size!.height)), getBadgePaint());
     canvas.restore();
   }
 
   Paint getBadgePaint() => Paint()
     ..isAntiAlias = true
     ..color = badgeColor;
-
-  Path buildBadgePath() => Path.combine(
-      PathOperation.difference,
-      Path()..addRect(Rect.fromLTRB(0, 0, badgeSize, badgeSize)),
-      Path()
-        ..moveTo(badgeSize, 0)
-        ..lineTo(0, badgeSize)
-        ..lineTo(badgeSize, badgeSize)
-        ..close());
 }
