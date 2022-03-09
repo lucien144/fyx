a57e5fedb386f8d913e744bbe57dc6ed762062a9
Jan Blasko <honza@lucien144.net>
2022-03-09 21:29:46 +0100

    fix: another locales added, default EN #205

--- lib/SkinnedApp.dart
+++ lib/SkinnedApp.dart
@@ -16,29 +16,30 @@ class SkinnedApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return CupertinoApp(
-      title: 'Fyx',
-      theme: Skin.of(context).theme.data,
-      home: MainRepository().credentials != null && MainRepository().credentials!.isValid ? HomePage() : LoginPage(),
-      debugShowCheckedModeBanner: FyxApp.isDev,
-      onUnknownRoute: (RouteSettings settings) => CupertinoPageRoute(builder: (_) => DiscussionPage(), settings: settings),
-      onGenerateRoute: FyxApp.routes,
-      navigatorKey: FyxApp.navigatorKey,
-      navigatorObservers: [
-        FyxApp.routeObserver,
-        FirebaseAnalyticsObserver(
-            analytics: FyxApp.analytics,
-            onError: (error) async => await Sentry.captureException(
-              error,
-            ))
-      ],
-      localizationsDelegates: [
-        GlobalMaterialLocalizations.delegate,
-        GlobalWidgetsLocalizations.delegate,
-        GlobalCupertinoLocalizations.delegate,
-      ],
-      supportedLocales: [
-        Locale('cs', ''),
-      ]
-    );
+        title: 'Fyx',
+        theme: Skin.of(context).theme.data,
+        home: MainRepository().credentials != null && MainRepository().credentials!.isValid ? HomePage() : LoginPage(),
+        debugShowCheckedModeBanner: FyxApp.isDev,
+        onUnknownRoute: (RouteSettings settings) => CupertinoPageRoute(builder: (_) => DiscussionPage(), settings: settings),
+        onGenerateRoute: FyxApp.routes,
+        navigatorKey: FyxApp.navigatorKey,
+        navigatorObservers: [
+          FyxApp.routeObserver,
+          FirebaseAnalyticsObserver(
+              analytics: FyxApp.analytics,
+              onError: (error) async => await Sentry.captureException(
+                    error,
+                  ))
+        ],
+        localizationsDelegates: [
+          GlobalMaterialLocalizations.delegate,
+          GlobalWidgetsLocalizations.delegate,
+          GlobalCupertinoLocalizations.delegate,
+        ],
+        supportedLocales: [
+          Locale('en', ''),
+          Locale('cs', ''),
+          Locale('sk', ''),
+        ]);
   }
 }
