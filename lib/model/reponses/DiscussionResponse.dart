5dff7fb6d6c5a7a46bb225e8aec22bd5129971d4
Jan Blasko <honza@lucien144.net>
2022-06-20 21:13:25 +0200

    feat: DisscusionResponse.discussionSpecificData

--- lib/model/reponses/DiscussionResponse.dart
+++ lib/model/reponses/DiscussionResponse.dart
@@ -1,8 +1,13 @@
 import 'package:fyx/model/Discussion.dart';
+import 'package:fyx/model/DiscussionContent.dart';
 import 'package:fyx/model/ResponseContext.dart';
+import 'package:fyx/model/enums/DiscussionTypeEnum.dart';
+
+enum DiscussionSpecificDataEnum { header }
 
 class DiscussionResponse {
   late final Discussion discussion; // TODO: Rename this to DiscussionCommon
+  Map<DiscussionSpecificDataEnum, List<DiscussionContent>>? discussionSpecificData;
   late final List posts;
   ResponseContext? context;
 
@@ -21,4 +26,19 @@ class DiscussionResponse {
     this.posts = json['posts'] ?? [];
     this.context = ResponseContext.fromJson(json['context']);
 
+    switch (this.discussion.type) {
+      case DiscussionTypeEnum.discussion:
+        this.discussionSpecificData = {
+          DiscussionSpecificDataEnum.header:
+              List.from(json['discussion_common']['discussion_specific_data']['header']).map((item) => DiscussionContent.fromJson(item)).toList()
+        };
+        break;
+      case DiscussionTypeEnum.event:
+        // TODO: Handle this case.
+        break;
+      case DiscussionTypeEnum.advertisement:
+        // TODO: Handle this case.
+        break;
+    }
+  }
 }
