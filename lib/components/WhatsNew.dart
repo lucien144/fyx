c03f8e437efb5cf281fbadd0091578c509005aed
lucien144 <honza@lucien144.net>
2023-06-26 17:53:47 +0200

    feat: tweaks #390

--- lib/components/WhatsNew.dart
+++ lib/components/WhatsNew.dart
@@ -1,39 +1,54 @@
-import 'package:flutter/widgets.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:fyx/model/MainRepository.dart';
+import 'package:fyx/pages/search_page.dart';
 import 'package:fyx/theme/skin/Skin.dart';
 import 'package:fyx/theme/skin/SkinColors.dart';
 import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
 
+class WhatsNewAction {
+  final VoidCallback action;
+  final String label;
+
+  WhatsNewAction({required this.action, required this.label});
+}
+
 class WhatsNew extends StatelessWidget {
   const WhatsNew({Key? key}) : super(key: key);
 
-  Widget item({required IconData icon, required String title, required String description, required context}) {
+  Widget item({required IconData icon, required String title, required String description, required context, WhatsNewAction? action}) {
     SkinColors colors = Skin.of(context).theme.colors;
 
-    return Padding(
-      padding: const EdgeInsets.only(bottom: 32),
-      child: Row(
-        children: [
-          Icon(icon, size: 40),
-          SizedBox(width: 20),
-          Expanded(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(title,
-                    style: TextStyle(
-                      fontSize: 19,
-                      fontWeight: FontWeight.bold,
-                      color: colors.primary
-                    )),
-                Text(
-                  description,
-                  softWrap: true,
-                )
-              ],
-            ),
-          )
-        ],
+    return GestureDetector(
+      onTap: action?.action,
+      child: Padding(
+        padding: const EdgeInsets.only(bottom: 32),
+        child: Row(
+          children: [
+            Icon(icon, size: 40),
+            SizedBox(width: 20),
+            Expanded(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(title,
+                      style: TextStyle(
+                        fontSize: 19,
+                        fontWeight: FontWeight.bold,
+                      )),
+                  Text(
+                    description,
+                    softWrap: true,
+                  ),
+                  if (action != null)
+                    Text(
+                      action.label,
+                      style: TextStyle(color: colors.primary, decoration: TextDecoration.underline),
+                    )
+                ],
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
@@ -50,7 +65,6 @@ class WhatsNew extends StatelessWidget {
         style: TextStyle(
           fontSize: 24,
           fontWeight: FontWeight.w800,
-          color: colors.primary,
         ),
         textAlign: TextAlign.center,
       ),
@@ -58,7 +72,7 @@ class WhatsNew extends StatelessWidget {
       Text(
         'Verze $version',
         textAlign: TextAlign.center,
-        style: TextStyle(color: colors.primary, fontFamily: 'JetBrainsMono', fontSize: 13),
+        style: TextStyle(fontFamily: 'JetBrainsMono', fontSize: 13),
       ),
       SizedBox(height: 24),
       item(
@@ -70,12 +84,19 @@ class WhatsNew extends StatelessWidget {
           context: context,
           icon: MdiIcons.magnify,
           title: 'Fulltextové hledání.',
-          description: 'Nově lze hledat ve všech příspěvcích napříč Nyxem. Funkce je dostupná z hlavního menu.'),
+          description: 'Další novinkou je fulltextové hledání příspěvků napříč Nyxem. Hledání je dostupné z hlavního menu.',
+          action: new WhatsNewAction(action: () {
+            Navigator.of(context).pop();
+            var arguments = SearchPageArguments(searchTerm: 'fyx');
+            Navigator.of(context, rootNavigator: true).pushNamed('/search', arguments: arguments);
+          }, label: 'Chci si vyzkoušet hledání.')),
       item(
           context: context,
           icon: MdiIcons.bug,
-          title: 'Opravy.',
-          description: 'Krátký spoiler, odpověď na inzerát, otevírání odkazů, indikace přečteno v poště, možnost zobrazení výsledků ankety, indikátor galerie a další...'),
+          title: 'Opravy a drobná vylepšení.',
+          description:
+              'Nehlasovat v anketě, neresetovat hodnocení při mínusu, krátký spoiler, ikona přečteno v pošte, odpověď na inzerát a další...'),
+      CupertinoButton(child: Text('Pokračovat'), onPressed: () => Navigator.of(context).pop(), color: colors.primary)
     ]);
   }
 }
